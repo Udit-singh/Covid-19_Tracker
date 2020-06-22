559c0a934d28e259e3a3fe03444487bb8089859c
Udit singh <Udit-singh@users.noreply.github.com>
2020-06-22 12:08:03 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Covid-19 Tracker</NavbarBrand>
+        </div>
+      </Navbar>
     </div>
-  );
+    )
+  }
 }
+
 
 export default App;
